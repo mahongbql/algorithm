02d1ceeedf3df20f1853bfb0c91d31837d0b246d
mahongbin <1318380423@qq.com>
2020-03-17 14:47:43 +0800

    作业

---
+++ src/main/java/com/algorithm/secondWeek/BracketIsValid.java
@@ -0,0 +1,70 @@
+package com.algorithm.secondWeek;
+
+import com.alibaba.druid.sql.visitor.functions.Char;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ */
+public class BracketIsValid {
+
+    public static void main(String[] args) {
+        System.out.println(myIsValid("[]"));
+    }
+
+    public static boolean myIsValid(String s) {
+        if(s.length() == 0) {
+            return true;
+        }
+
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(map.containsKey(c)) {
+                stack.push(c);
+                continue;
+            }
+
+            if(stack.isEmpty() || map.get(stack.pop()) != c) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
