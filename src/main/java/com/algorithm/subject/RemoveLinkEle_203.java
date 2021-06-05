2e0933db7a5517715c8147154afcb3880f5cd002
mahongbin <1318380423@qq.com>
2021-06-05 14:14:55 +0800

    692

--- src/main/java/com/algorithm/subject/RemoveLinkEle_203.java
+++ src/main/java/com/algorithm/subject/RemoveLinkEle_203.java
@@ -5,8 +5,16 @@ package com.algorithm.subject;
  * @Date: 2021-06-05 14:09
  * @Description: 移除链表元素
  *
+ * 删除给定链表中等于给定值 val 的节点，并返回新的头节点
+ * 例子：
+ * 【1】输入：head = [1,2,6,3,4,5,6], val = 6
+ *     输出：[1,2,3,4,5]
  *
+ * 【2】输入：head = [], val = 6
+ *     输出：[]
  *
+ * 【3】输入：head = [4,4,4,4,4], val = 4
+ *     输出：[]
  */
 public class RemoveLinkEle_203 {
 }
